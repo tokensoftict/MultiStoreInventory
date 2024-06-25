4ad408aeccc7e6ace8b6f85a5fdff6b87660f2bb
Yusuf Olatunji <yoyforever@gmail.com>
2024-06-25 08:28:34 +0100

    fixing payment issue

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -89,6 +89,17 @@ class Payment extends Model
     }
 
 
+    public function getTotalPaidAttribute()
+    {
+        return $this->paymentMethodTable->sum(function($payment){
+            if($payment['payment_method_id'] !== 4){
+                return $payment['amount'];
+            }else{
+                return 0;
+            }
+        });
+    }
+
     public function invoice(){
 
         return $this->morphTo();
