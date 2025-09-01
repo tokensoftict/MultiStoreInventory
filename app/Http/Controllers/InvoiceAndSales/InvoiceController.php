28c75778c63196134a4d8eb5212c4be041757ab4
Yusuf Olatunji <yoyforever@gmail.com>
2025-09-01 11:30:49 +0100

    fix customer issue

--- app/Http/Controllers/InvoiceAndSales/InvoiceController.php
+++ app/Http/Controllers/InvoiceAndSales/InvoiceController.php
@@ -47,6 +47,7 @@ class InvoiceController extends Controller
     public function new(){
         $data = [];
         $data['customers'] = Customer::query()->get();
+        $data['customer'] = Customer::query()->find(1);
         $data['payments'] = PaymentMethod::all();
         $data['banks'] = BankAccount::where('status',1)->get();
         $data['settings'] =  $this->settings;
